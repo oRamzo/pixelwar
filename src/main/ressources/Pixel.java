34b50c489f85b790164ed247efd23fd71fe8e931
oRamzo <rami17mahmoud@hotmail.com>
2026-03-20 18:00:19 +0100

    Vraie fin du projet

--- src/main/ressources/Pixel.java
+++ src/main/ressources/Pixel.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 public class Pixel implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    //types de pixels pour diff les messages
     public enum Type {
         PIXEL,
         UPDATE,
@@ -21,6 +22,7 @@ public class Pixel implements Serializable {
         this(type, row, col, couleurHex, null, 0);
     }
 
+    //constructeur pixel
     public Pixel(Type type, int row, int col, String couleurHex, String pseudo, int secondesRestantes) {
         this.type = type;
         this.row = row;
@@ -30,22 +32,27 @@ public class Pixel implements Serializable {
         this.secondesRestantes = secondesRestantes;
     }
 
+    //demande un pixel
     public static Pixel pixelDemande(int row, int col, String couleurHex) {
         return new Pixel(Type.PIXEL, row, col, couleurHex, null, 0);
     }
 
+    //update un pixel
     public static Pixel pixelUpdate(int row, int col, String couleurHex, String pseudo) {
         return new Pixel(Type.UPDATE, row, col, couleurHex, pseudo, 0);
     }
 
+    //etat d'un pixel
     public static Pixel pixelState(int row, int col, String couleurHex, String pseudo, int secondesRestantes) {
         return new Pixel(Type.STATE, row, col, couleurHex, pseudo, secondesRestantes);
     }
 
+    //pixel occupé
     public static Pixel pixelBusy(int row, int col, String pseudo, int secondesRestantes) {
         return new Pixel(Type.BUSY, row, col, null, pseudo, secondesRestantes);
     }
 
+    //getters
     public Type getType() {
         return type;
     }
